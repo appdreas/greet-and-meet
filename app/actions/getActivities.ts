cc7947e819ad13708a608f6179a0844840b43b51
Andreas <ahultqvist17@gmail.com>
2024-12-16 16:38:12 +0100

    compile fix

--- app/actions/getActivities.ts
+++ app/actions/getActivities.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createAdminClient } from "@/config/appwrite";
-import { revalidatePath } from "next/cache";
+//import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 async function getActivities() {
